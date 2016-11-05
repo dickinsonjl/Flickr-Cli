5e00982a26993bd917587ca2c6e93640cad41463
John Dickinson <dickinsonjl@gmail.com>
2016-11-05 15:30:39 +0000

    adding extensive mode option, upload subdirs to separate photosets

--- flickr_up.py
+++ flickr_up.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/python -u
 # encoding: utf8
 import sys
 import os
@@ -8,14 +8,30 @@ import flickrapi
 import flickr_cli
 from ConfigParser import ConfigParser
 
-config = ConfigParser()
-config.read('flickr.config')
-api_key = config.get('flickr', 'key')
-secret = config.get('flickr', 'secret')
-
 
 def photoset_default_title(d):
     return os.path.basename(os.path.normpath(d))
+
+
+def process_single_directory(d, ps, t):
+    print d, t, ps
+
+    config = ConfigParser()
+    config.read('flickr.config')
+    api_key = config.get('flickr', 'key')
+    secret = config.get('flickr', 'secret')
+    flickr = flickrapi.FlickrAPI(api_key, secret)
+
+    if not flickr.token_valid(perms=u'write'):
+        flickr.get_request_token(oauth_callback=u'oob')
+        authorize_url = flickr.auth_url(perms=u'write')
+        webbrowser.open_new_tab(authorize_url)
+        verifier = unicode(raw_input('Verifier code: '))
+        flickr.get_access_token(verifier)
+
+    # upload = flickr_cli.DirectoryFlickrUpload(flickr)
+    upload = flickr_cli.FamilyDirectoryUpload(flickr)
+    upload(directory=d, pset=ps, tags=t)
 
 
 parser = OptionParser(version="1.0")
@@ -23,6 +39,10 @@ parser = OptionParser(version="1.0")
 parser.add_option("-d", "--directory", dest="directory",
                   help="The directory from which you wish to copy the files",
                   metavar="DIRECTORY")
+
+parser.add_option("-e", "--extensive", dest="extensive",
+                  help="The directory holding multiple sub-directories which will be unique photosets",
+                  metavar="EXTENSIVE")
 
 parser.add_option("-p", "--photoset",
                   dest="photoset", default=False,
@@ -53,35 +73,33 @@ if args:
     sys.exit(0)
 
 if not options.directory:
-    print "ERROR"
-    print "You must pass a directory.\n (example goes here)"
-    sys.exit(0)
+    if not options.extensive:
+        print "ERROR"
+        print "You must pass a directory.\n (-d or -e /my/folder etc.)"
+        sys.exit(0)
 
-directory = options.directory
 tags = options.tags or ""
-photoset = options.photoset or photoset_default_title(options.directory)
-
-if not options.photoset or not options.tags:
-    print "WARNING"
-
-if not options.photoset:
-    print "You did not pass a name for the photoset.\n"
-    print "We will use the photoset title '{}'".format(photoset)
 
 if not options.tags:
-    print "You did not pass any tags.\n"
+    print "WARNING: You did not pass any tags.\n"
 
-print directory, tags, photoset
+if options.extensive:
+    extensive = options.extensive or "."
+    print "Extensive mode."
 
-flickr = flickrapi.FlickrAPI(api_key, secret)
+    for dirpath, dirs, files in os.walk(extensive):
+        for d in dirs:
+            print 'dir:', d
+            this_fullpath = os.path.abspath(dirpath) + '/' + d
+            print 'fullpath:', this_fullpath
+            photoset = photoset_default_title(this_fullpath)
+            print "We will use the photoset title '{}'".format(photoset)
+            process_single_directory(this_fullpath, photoset, tags)
+else:
+    directory = options.directory
+    photoset = options.photoset or photoset_default_title(options.directory)
 
-if not flickr.token_valid(perms=u'write'):
-    flickr.get_request_token(oauth_callback=u'oob')
-    authorize_url = flickr.auth_url(perms=u'write')
-    webbrowser.open_new_tab(authorize_url)
-    verifier = unicode(raw_input('Verifier code: '))
-    flickr.get_access_token(verifier)
-
-# upload = flickr_cli.DirectoryFlickrUpload(flickr)
-upload = flickr_cli.FamilyDirectoryUpload(flickr)
-upload(directory=directory, pset=photoset, tags=tags)
+    if not options.photoset:
+        print "WARNING: You did not pass a name for the photoset.\n"
+        print "We will use the photoset title '{}'".format(photoset)
+    process_single_directory(directory, photoset, tags)
